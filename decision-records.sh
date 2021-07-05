d5e96193f0d4f0e2223dec9a15bbb48eeaa06469
Jon Spriggs <jon@sprig.gs>
2021-07-06 00:37:30 +0100

    A few moves around but otherwise a big blob of code

--- decision-records.sh
+++ decision-records.sh
@@ -57,7 +57,7 @@ function info() {
 }
 
 function warning() {
-    msg "${PURPLE}[Warning]: $1${NOFORMAT}"
+  msg "${PURPLE}[Warning]: $1${NOFORMAT}"
 }
 
 function error() {
@@ -74,39 +74,32 @@ function error() {
 }
 
 ##############################
-# ROOT
+# ROOT & Records
 ##############################
 
-function decision_record_config_path() {
-  invoke "decision_record_config_path(${1-})"
-  if [ -z "$decision_record_config_path" ]
-  then
-    local config_file
-    local config_path
-    if [ -n "${1-}" ]
+function _absolute_directory() {
+  invoke "absolute_directory($1)"
+  # Change to this directory
+  if ! cd "$1"
+  then 
+    if [ "$sourced" -eq 0 ]
     then
-      config_file="$1/.decisionrecords-config"
+      return 1
     else
-      config_file="$(find_root_path)/.decisionrecords-config"
-    fi
-
-    config_path="doc/decision_records" # Default value
-    if grep -E '^records=' "$config_file" >/dev/null
-    then
-      config_path="$(grep -E '^records=' "$config_file" | cut -d= -f2)"
+      exit 1
     fi
   fi
-
-  invoke_response "decision_record_config_path(${1-})" "$config_path"
-  echo "$config_path"
+  # Print the path of this directory, resolving any symlinks
+  result="$(pwd -P)"
+  invoke_response "absolute_directory($1)" "$result"
+  echo "$result"
 }
 
 function find_root_path() {
   invoke "find_root_path()"
   if [ -z "$decision_records_root" ]
   then
-    this_directory="."
-    absolute_directory="$(absolute_directory "$this_directory")"
+    absolute_directory="$(_absolute_directory ".")"
     while [ "$absolute_directory" != "/" ] && [ -z "$decision_records_root" ]
     do
       if [ -f "${absolute_directory}/.adr-dir" ] && [ -d "${absolute_directory}/$(cat "${absolute_directory}/.adr-dir")" ]
@@ -127,14 +120,13 @@ function find_root_path() {
         info "Found Decision Record root path \`$decision_records_root\` (via default new path)"
       else
         debug "Not found in this path, going up!"
-        this_directory="../$this_directory"
-        absolute_directory="$(absolute_directory "$this_directory")"
+        absolute_directory="$(_absolute_directory "$absolute_directory/..")"
       fi
     done
 
     if [ -z "$decision_records_root" ]
     then
-      error "No decision records root found. Have you run \`$0 init\`?" 1
+      error "No decision records root found. Have you run \`$(basename "$0") init\`?" 1
     else
       invoke_response "find_root_path()" "$decision_records_root"
       echo "$decision_records_root"
@@ -142,14 +134,196 @@ function find_root_path() {
   fi
 }
 
-function absolute_directory() {
-  invoke "absolute_directory($1)"
-  # Change to this directory
-  cd "$(dirname "$1")" || return
-  # Print the path of this directory, resolving any symlinks
-  result="$(pwd -P)"
-  invoke_response "absolute_directory($1)" "$result"
-  echo "$result"
+function find_record_path() {
+  invoke "find_record_path()"
+  if [ -z "$decision_records_path" ]
+  then
+    this_directory="$(find_root_path)"
+    if [ -f "${this_directory}/.adr-dir" ] && [ -d "${this_directory}/$(cat "${this_directory}/.adr-dir")" ]
+    then
+      decision_records_path="$(cat "${this_directory}/.adr-dir")"
+      info "Found Decision Record path for records \`$decision_records_root\` (via .adr-dir)"
+    elif [ -f "${this_directory}/.decisionrecords-config" ] && [ -d "${this_directory}/$(decision_record_config_path "${this_directory}")" ]
+    then
+      decision_records_path="$(decision_record_config_path "${this_directory}")"
+      info "Found Decision Record path for records \`$decision_records_root\` (via .decisionrecords-config)"
+    elif [ -d "${this_directory}/doc/adr" ]
+    then
+      decision_records_path="doc/adr"
+      info "Found Decision Record path for records \`$decision_records_root\` (via default old path)"
+    elif [ -d "${this_directory}/doc/decision_records" ]
+    then
+      decision_records_path="doc/decision_records"
+      info "Found Decision Record path for records \`$decision_records_root\` (via default new path)"
+    fi
+  fi
+  invoke_response "find_record_path()" "$decision_records_path"
+  echo "$decision_records_path"
+}
+
+##############################
+# Config Items
+##############################
+
+function decision_record_config_path() {
+  invoke "decision_record_config_path(${1-})"
+  if [ -z "$decision_record_config_path" ]
+  then
+    local config_file
+    local config_path
+    if [ -n "${1-}" ]
+    then
+      config_file="$1/.decisionrecords-config"
+    else
+      config_file="$(find_root_path)/.decisionrecords-config"
+    fi
+
+    info "Using config file: $config_file"
+        
+    config_path="doc/decision_records" # Default value
+    if [ -f "$config_file" ] && grep -E '^records=' "$config_file" >/dev/null 2>&1
+    then
+      config_path="$(grep -E '^records=' "$config_file" | head -n 1 | cut -d= -f2)"
+    else
+      info "Configuration value 'records' not found in $config_file. Using the default value, \`$config_path\`."
+    fi
+  fi
+
+  decision_record_config_path="$config_path"
+  invoke_response "decision_record_config_path(${1-})" "$decision_record_config_path"
+  echo "$decision_record_config_path"
+}
+
+# shellcheck disable=SC2120
+function decision_record_config_template_dir() {
+  invoke "decision_record_config_template_dir()"
+  if [ -z "$decision_record_config_template_dir" ]
+  then
+    local config_file
+    local config_path
+    config_file="$(find_root_path)/.decisionrecords-config"
+
+    config_path="$(find_record_path)/.templates" # Default value
+    if [ -f "$config_file" ] && grep -E '^templatedir=' "$config_file" >/dev/null 2>&1
+    then
+      config_path="$(grep -E '^templatedir=' "$config_file" | head -n 1 | cut -d= -f2)"
+    else
+      info "Configuration value 'templatedir' not found in $config_file. Using the default value, \`$config_path\`."
+    fi
+  fi
+
+  local find_root_path
+  find_root_path="$(find_root_path)"
+
+  if [ ! -e "$find_root_path/$config_path" ]
+  then
+    error "Template Directory \`$config_path\` is missing. Unable to proceed." 1
+  else
+    invoke_response "decision_record_config_template_dir()" "$config_path"
+    echo "$config_path"
+  fi
+
+}
+
+function decision_record_config_template_file() {
+  invoke "decision_record_config_template_file()"
+  if [ -z "$decision_record_config_template_file" ]
+  then
+    local config_file
+    local config_path
+    config_file="$(find_root_path)/.decisionrecords-config"
+
+    config_path="template" # Default value
+    if [ -f "$config_file" ] && grep -E '^template=' "$config_file" >/dev/null 2>&1
+    then
+      config_path="$(grep -E '^template=' "$config_file" | head -n 1 | cut -d= -f2)"
+      if [ "$(grep -c -E '^template=' "$config_file")" -gt 1 ]
+      then
+        warning "Multiple Template configuration values detected. First value used. Please check your config file \`$config_file\` for values starting template="
+      fi
+    else
+      info "Configuration value 'template' not found in $config_file. Using the default value, \`$config_path\`."
+    fi
+  fi
+
+  local find_root_path
+  local decision_record_config_template_dir
+  find_root_path="$(find_root_path)"
+  decision_record_config_template_dir="$(decision_record_config_template_dir)"
+
+  if [ -n "$decision_record_config_template_dir" ]
+  then
+    if [ ! -e "$find_root_path/$decision_record_config_template_dir/$config_path.md" ]
+    then
+      error "Template \`$decision_record_config_template_dir/$config_path.md\` is missing. Unable to proceed." 1
+    else
+      invoke_response "decision_record_config_template_file()" "$config_path"
+      echo "$config_path"
+    fi
+  fi
+}
+
+function decision_record_config_template_language() {
+  invoke "decision_record_config_template_language()"
+  if [ -z "$decision_record_config_template_language" ]
+  then
+    local config_file
+    local config_language
+    config_file="$(find_root_path)/.decisionrecords-config"
+
+    config_language=""
+    if [ -f "$config_file" ] && grep -E '^language=' "$config_file" >/dev/null 2>&1
+    then
+      config_language="$(grep -E '^language=' "$config_file" | head -n 1 | cut -d= -f2)"
+      if [ "$(grep -c -E '^language=' "$config_file")" -gt 1 ]
+      then
+        warning "Multiple Template configuration values detected. First value used. Please check your config file \`$config_file\` for values starting language="
+      fi
+    else
+      info "Configuration value 'language' not found in $config_file. Using the default value, \`$config_path\`."
+    fi
+  fi
+
+  info "Using language $config_language"
+
+  local find_root_path
+  local decision_record_config_template_dir
+  local decision_record_config_template_file
+  find_root_path="$(find_root_path)"
+  decision_record_config_template_dir="$(decision_record_config_template_dir)"
+  decision_record_config_template_file="$(decision_record_config_template_file)"
+  if [ -n "$decision_record_config_template_dir" ] && [ -n "$decision_record_config_template_file" ]
+  then
+    if [ -n "$config_language" ] && [ -e "$find_root_path/$decision_record_config_template_dir/$decision_record_config_template_file.$config_language.md" ] && [ -e "$find_root_path/$decision_record_config_template_dir/$decision_record_config_template_file.$config_language.ref" ]
+    then
+      invoke_response "decision_record_config_template_language()" "$config_language"
+      echo "$config_language"
+    elif [ -n "$config_language" ] && [ -e "$find_root_path/$decision_record_config_template_dir/$decision_record_config_template_file.$(echo "$config_language" | cut -d_ -f 1).md" ] && [ -e "$find_root_path/$decision_record_config_template_dir/$decision_record_config_template_file.$(echo "$config_language" | cut -d_ -f 1).ref" ]
+    then
+      invoke_response "decision_record_config_template_language()" "$(echo "$config_language" | cut -d_ -f 1)"
+      echo "$config_language" | cut -d_ -f 1
+    else
+      invoke_response "decision_record_config_template_language()" "<none>"
+      echo ""
+    fi
+  fi
+}
+
+##############################
+# Create Items
+##############################
+
+function get_template() {
+  invoke "get_template()"
+  if [ -z "$decision_record_config_template_path" ]
+  then
+    local decision_record_config_template_dir
+    local decision_record_config_template_file
+    local decision_record_config_template_language
+    decision_record_config_template_dir="$(decision_record_config_template_dir)"
+    decision_record_config_template_file="$(decision_record_config_template_file)"
+    decision_record_config_template_language="$(decision_record_config_template_language)"
+  fi
 }
 
 function main() {
@@ -160,4 +334,6 @@ if [[ "${BASH_SOURCE[0]}" == "${0}" ]]
 then
   sourced_off
   main
+else
+  unset -v decision_record_config_template_file decision_record_config_template_dir decision_record_config_path decision_records_path decision_records_root
 fi
